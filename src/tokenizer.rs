1a28bcbd2b8335ce007c27a90e9c87a3664087f2
Nathan Agbomedarho <nathanagbomed@proton.me>
2024-09-08 17:18:36 -0400

    Implement scanning for Division operatos, whitespcae, multi-line erros, string and number literals.

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,5 +1,4 @@
 use std::fmt;
-use std::io::{self, Write};
 
 pub struct Tokenizer <'a> {
     source: &'a str,
@@ -7,7 +6,6 @@ pub struct Tokenizer <'a> {
     start:usize,
     current: usize,
     line: usize,
-    errors: Vec<String>,
     pub has_error: bool,
 }
 
@@ -31,6 +29,9 @@ pub enum TokenType {
     GreaterEqual,
     Less,
     LessEqual,
+    Slash,
+    String,
+    Number,
     EOF,
     WhiteSpace,
 }
@@ -75,6 +76,9 @@ impl fmt:: Display for TokenType {
             TokenType:: GreaterEqual => write!(f, "GREATER_EQUAL"),
             TokenType:: Less => write!(f, "LESS"),
             TokenType:: LessEqual => write!(f, "LESS_EQUAL"),
+            TokenType:: Slash => write!(f, "SLASH"),
+            TokenType:: String => write!(f, "STRING"),
+            TokenType:: Number => write!(f, "NUMBER"),
             TokenType:: EOF => write!(f, "EOF"),
             TokenType:: WhiteSpace => write!(f, "WHITESPACE"),
         }
@@ -89,7 +93,6 @@ impl<'a> Tokenizer<'a> {
             start: 0,
             current: 0,
             line: 1,
-            errors: Vec::new(),
             has_error: false,
 
         }
@@ -113,6 +116,22 @@ impl<'a> Tokenizer<'a> {
 
     }
 
+    pub fn report_error_string(&mut self, unexpected_string: String) {
+
+        let error_msg = format! (
+
+            "[line {}] Error: Unterminated string.{}\n",
+            self.line,
+            unexpected_string,
+        );
+
+        eprint!("{}", error_msg);
+        self.has_error = true;
+
+
+
+    }
+
 
     pub fn scan_tokens(&mut self) -> Vec<Token> {
         while !self.is_at_end() {
@@ -126,11 +145,6 @@ impl<'a> Tokenizer<'a> {
             literal: None,
         });
         
-        // if self.has_error {
-        //     for error in &self.errors {
-        //         eprint!("{}", error);
-        //     }
-        // }
         self.tokens.clone()
     }
 
@@ -181,16 +195,145 @@ impl<'a> Tokenizer<'a> {
                 }
 
             },
+            '/' => {
+                if self.match_next('/') {
+
+                    while self.peek() != '\n' && !self.is_at_end() {
+
+                        self.advance();
+                    }
+                }
+                else {
+                    self.add_token(TokenType::Slash);
+                }
+                
+            },
+            '"' => {
+                self.string();
+            }
             ' ' | '\r' | '\t' => self.add_token(TokenType::WhiteSpace), // Ignore whitespace
             '\n' => {
                 self.line += 1;
                 self.add_token(TokenType::WhiteSpace);
             }
             _ => {
-                self.report_error(c);
+
+                if self.is_digit(c) {
+
+                    self.is_number();
+                }
+                else {
+                    self.report_error(c);
+                }
+                
             }
         }
     }
+
+
+    fn is_digit(&self, c: char) -> bool {
+
+        // c.is_ascii_digit()
+
+        return c >= '0' && c <= '9';
+    }
+
+    fn is_number(&mut self) {
+        
+        let start = self.current - 1;
+
+        let mut has_decimal = false;
+
+    
+        while self.is_digit(self.peek()) {
+            self.advance();
+        }
+        // Look for fractional part
+        if self.peek() == '.' && self.is_digit(self.peek_next()) {
+            has_decimal = true;
+            // Consume the "."
+            self.advance();
+
+            while self.is_digit(self.peek()) {
+                self.advance();
+            }
+        }
+
+        let number_str = &self.source[self.start..self.current];
+
+        let number: f64 = number_str.parse().unwrap_or(0.0);
+
+        let formatted_number = if has_decimal {
+            number.to_string()
+        }
+        else {
+
+            format!("{}.0", number)
+        };
+
+
+        self.add_token_with_literal(TokenType::Number, Some(formatted_number.clone()));
+
+
+    }
+
+    fn string(&mut self){
+
+        let start_quote = self.current - 1;
+
+        while self.peek() != '"' && !self.is_at_end() {
+
+            if self.peek() == '\n' {
+
+                self.line += 1;
+            }
+
+            self.advance();
+        }
+
+        if self.is_at_end() {
+
+            self.report_error_string("".to_string());
+            return;
+        }
+
+        self.advance();
+        
+        let string_content = &self.source[start_quote + 1..self.current - 1];
+
+        self.add_token_with_literal(TokenType:: String, Some(string_content.to_string()));
+      
+
+
+    }
+
+    fn add_token_with_literal(&mut self, token_type: TokenType, literal: Option<String>){
+
+        let text = &self.source[self.start..self.current];
+
+        self.tokens.push(Token {
+            token_type,
+            lexeme: text.to_string(),
+            literal,
+        })
+    }
+
+ 
+    fn peek(&self) -> char{
+
+       self.source.chars().nth(self.current).unwrap_or('\0')
+    }
+
+    pub fn peek_next(&self) -> char {
+
+        if self.current + 1 >= self.source.len() {
+
+            return '\0';
+        }
+
+        return self.source.chars().nth(self.current + 1).unwrap_or('\0');
+    }
+
 
     fn match_next(&mut self, expected: char) -> bool {
 
@@ -210,7 +353,7 @@ impl<'a> Tokenizer<'a> {
     }
 
     fn advance(&mut self) -> char {
-        let current_char = self.source.chars().nth(self.current).unwrap();
+        let current_char = self.source.chars().nth(self.current).unwrap_or('\0');
         self.current += 1;
         current_char
     }
